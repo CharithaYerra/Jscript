1217d4a635f4cfe331e3413456e0db2c897a8349
grumpchick <divyacharitha20@gmail.com>
2025-05-16 17:25:20 +0530

    start

---
+++ Jscript.js
@@ -0,0 +1,44 @@
+ console.log("Hello world"); //string and constant in console
+ console.log(123);
+ 
+//document is used for writing in the web page 
+document.write("Hello world<br>");
+document.write(123+"<br>");//br is used to break the sentence
+document.write("Hello");
+document.write(123);
+
+//variable let constant
+//variable can be assigned and declared many times
+var customerID = 10;
+var customerID = 20;
+
+console.log(customerID);
+
+let rollNo= 50;
+    rollNo =30;//reassing the data
+
+const phno=3429874349;
+
+console.log(rollNo);
+console.log(phno);
+
+//primitive date types
+
+let age=10;
+let name="java";
+let Matter=`${name} is a programming language which is usefull for web development`;
+console.log(Matter);
+
+let alive=true;
+console.log(alive);
+
+let sym1 = Symbol("javascript");
+console.log(sym1);
+
+
+
+
+
+
+
+
---
+++ Jscript1.js
@@ -0,0 +1,6 @@
+let employee=["narmada","isha","durga","pavani"];//arrays
+       console.log(employee[0]);
+ 
+       let empId=["emp123","emp567","emp238","emp986"];
+       console.log(`${employee[2]} ${empId[2]} `);
+       console.log(employee[2], empId[2]);
