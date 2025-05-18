63e5471e6cd7132ecd60513b5ff438c7f34d1c1a
grumpchick <divyacharitha20@gmail.com>
2025-05-18 18:40:17 +0530

    change

---
+++ functions/Jscript.js
@@ -0,0 +1,29 @@
+
+//function usage
+function School() {
+    let Teachers = ["Swapna", "siri", "Karthik"];
+    let Classes = [5, 6, 7, 8];
+    let Address = { dno: "15-188", Street: "NGO colony", city: "kadapa" };
+    console.log(Teachers);
+    console.log(Classes);
+    console.log(Address);
+}//function defination
+School();//function calling
+
+//styling the console using %c styling directive
+
+console.log("%c  ","background-color:lightgreen");
+console.log("%cSchool Details are given above","color:Gray");
+
+//wrap the styles in the function
+//function defination having function calling
+
+function black(){
+    console.log("%c  ","background-color:black");
+}
+
+function section1(){
+    console.log("%c  ","background-color:blue");
+    black();  //function calling inside a function
+}
+section1(); //calling the section
