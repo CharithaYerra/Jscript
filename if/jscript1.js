baee26578e9bada85506d5b6789f142866ef1506
grumpchick <divyacharitha20@gmail.com>
2025-05-28 13:34:41 +0530

    coomit

--- if/jscript1.js
+++ if/jscript1.js
@@ -38,3 +38,28 @@ function greet(lang,fun){
 console.log(greet("english",function(lang){
     return "this is "+lang+" language";
 }));
+
+
+
+function Courses(names){
+    names();
+    console.log(names);
+}
+
+Courses(function(){
+    let Names=["BZC","BTZC","BCOM"];
+    document.write(Names);
+    
+});
+
+
+
+
+function InterCourse(Ids){
+    Ids("Mpc","Bipc");
+}
+
+InterCourse(function(...names)
+{
+    console.log(`the courses available in inter are %c${names}`,"color:pink");
+});
