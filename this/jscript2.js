8d02db5fbfda50a43200ba0d26eae79d5ef86bbc
grumpchick <divyacharitha20@gmail.com>
2025-05-28 16:23:49 +0530

    commit

--- this/jscript2.js
+++ this/jscript2.js
@@ -26,14 +26,20 @@ User.prototype.isAdult = function() {
     return this.age >= 18;
 };
 
+
+//we cannot use the arrow function here as it doesnot take function variables
+
 User.prototype.details = function() {
     console.log(`${this.name} is ${this.age} years old`);
 };
 
 // Create instance
+
 let user1 = new User("siri", 21);
 
 // Use prototype methods
+
+
 user1.greet();       // Output: Hello, my name is siri
 console.log(user1.isAdult()); // Output: true
 user1.details();     // Output: siri is 21 years old
