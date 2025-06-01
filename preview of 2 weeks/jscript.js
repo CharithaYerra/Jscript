3968016e8641446da7a2b9eee5cbd08d095fd275
grumpchick <divyacharitha20@gmail.com>
2025-06-01 23:01:24 +0530

    commit

---
+++ preview of 2 weeks/jscript.js
@@ -0,0 +1,32 @@
+//let const var
+
+let date="31-05-2025";
+
+date="01-06-2025";
+
+const heading="practice";
+document.write(heading);
+
+console.log("today date is : ",date);
+
+// var age=20;
+// var age=30;
+
+// console.log(age);
+
+//primitive data types
+
+//string,boolean,number,null,undefined,symbol
+
+let name="divya";
+let physicallydisabled=false;
+let age=25;
+
+console.log("the name,physicallydisabled,age data type is : ",typeof(name),typeof(physicallydisabled),typeof(age));
+
+let gmail;
+console.log("gmail is: ",gmail);
+
+let phoneno=null;
+console.log("null is: ",phoneno);
+
