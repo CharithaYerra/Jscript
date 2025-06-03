25de2104a3cc321727c20e0054f6230e3818df79
grumpchick <divyacharitha20@gmail.com>
2025-06-03 12:45:53 +0530

    commit

---
+++ logical operators/jscript2.js
@@ -0,0 +1,35 @@
+//logical operators
+
+//logical and -  &&
+
+if(5 && "string"){
+    console.log("In logical operator if the two conditions is true then result is true")
+}
+
+if(0 && 1){
+    console.log("the output of false and true in logical && is false");
+}
+
+if(false && 0){
+    
+    console.log("if both statement are false the output will be false");
+
+}
+
+
+//logical or-- ||
+
+if(5 || "string"){
+    console.log("In logical operator if the two conditions is true then result is true")
+}
+
+if(0 || 1){
+    console.log("the output of false and true in logical || is true");
+}
+
+if(false || 0){
+    
+    console.log("if both statement are false the output will be false");
+
+}
+
---
+++ logical operators/jscript3.js
@@ -0,0 +1,23 @@
+let rollNo=53;
+let name="siri";
+
+
+//logical and ----> &&
+
+console.log("the results of logical and are:")
+
+console.log(rollNo=='53' && name=="siri");
+console.log(rollNo=='53' && name=="Siri");
+console.log(rollNo==54 && name=="siri");
+console.log(rollNo==54 && name=="Siri");
+
+
+//logical or ---->  ||
+
+console.log("the results of logical OR are:")
+
+
+console.log(rollNo=='53' || name=="siri");
+console.log(rollNo=='53' || name=="Siri");
+console.log(rollNo==54 || name=="siri");
+console.log(rollNo==54 || name=="Siri");
