43ff9d2530bcceac72ec8e747341c8fc85d0712c
grumpchick <divyacharitha20@gmail.com>
2025-06-03 12:28:41 +0530

    commit

---
+++ logical operators/jscript1.js
@@ -0,0 +1,44 @@
+if(1){
+    console.log("1 is true ");
+}
+
+if(0){
+    console.log("0 is false");
+}
+
+else if(35){
+    console.log("let us see the numbers are true or false")
+}
+
+
+
+if(undefined){
+    console.log("undefined is false");
+}
+
+if(null){
+    console.log("numll is false");
+}
+
+if("hi"){
+    console.log("string is true");
+}
+let hi;
+if(hi){
+    console.log("undefined variable is false");
+}
+
+let greet="hello";
+if(greet){
+    console.log("variable with value of string is true");
+}
+
+let number="45";
+if(number){
+    console.log("the variable with number in string format is true");
+}
+
+let value=45;
+if(value){
+    console.log("variable with number is true");
+}
