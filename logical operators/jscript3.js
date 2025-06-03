6d919744e41b2464eaafcf57893797643e7ee7a5
grumpchick <divyacharitha20@gmail.com>
2025-06-03 12:55:27 +0530

    commit

--- logical operators/jscript3.js
+++ logical operators/jscript3.js
@@ -21,3 +21,27 @@ console.log(rollNo=='53' || name=="siri");
 console.log(rollNo=='53' || name=="Siri");
 console.log(rollNo==54 || name=="siri");
 console.log(rollNo==54 || name=="Siri");
+
+
+
+let apparID=487454658789454;
+let password="Dkuengf5578";
+
+if(apparID==487454658789454  && password=="Dkuengf5578")
+{
+    console.log("Successfully logged in to your account");
+}
+else if(apparID==487454658789454  || password=="Dkuengf5578")
+{
+    if(apparID==487454658789454 ){
+        console.log("Incorrect Password");
+    }
+    else{
+        console.log("Incorrect Account number");
+    }
+}
+else{
+    console.log("Incorrect Details");
+}
+
+
