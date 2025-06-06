a79b77e365556e93c0e64ef6766604b4757264d9
grumpchick <divyacharitha20@gmail.com>
2025-06-06 14:41:16 +0530

    commit

--- for/jscript3.js
+++ for/jscript3.js
@@ -8,3 +8,19 @@ while (count <= 5) {
 
 
 
+console.time("time start");
+function sleep(){
+  let start=new Date().getTime();
+  let end=start+1000;
+  while(new Date().getTime()<end){
+
+  }
+}
+function stopwatch(seconds){
+for(let i=seconds;i>=1;i--){
+  console.log("no of seconds remaining is "+i);
+  sleep();
+}
+}
+stopwatch(5);
+console.timeEnd("time start");
