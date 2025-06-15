a9c84956af06d21264f07a3024ef6b556b9490b2
grumpchick <divyacharitha20@gmail.com>
2025-06-15 21:56:16 +0530

    questions

--- interview/jscript.js
+++ interview/jscript.js
@@ -2,3 +2,11 @@ console.log("A"-1);//nan
 console.log("divya"+100);
 console.log("2"+2-"2");
 
+console.log(3+3+"3");
+console.log("3"+3+3);
+
+//speak audio
+let text="hello world";
+ const utterance = new SpeechSynthesisUtterance(text); // create speech object
+ utterance.lang = 'en-US';
+  speechSynthesis.speak(utterance); // speak it
